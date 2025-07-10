ce536a64fea10d24804bfb64581f6ede38749f6f
ermias <ermiaskindalem97@gmail.com>
2025-07-11 00:59:24 +0300

    python script for basic operation added

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,7 @@
+number1 = 45
+number2 = 9
+
+
+print(f"Addition of {number1} and {number2} is {number1 + number2}.")
+print(f"Subtraction of {number1} and {number2} is {number1 - number2}.")
+print(f"Multiplication of {number1} and {number2} is {number1 * number2}.")
