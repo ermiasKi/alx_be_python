714389e569407ccfd9cb1122c6358f5d7e0c8d45
ermias <ermiaskindalem97@gmail.com>
2025-07-18 19:40:12 +0300

    week4's task done

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -4,7 +4,26 @@ priority = input('Priority (high/medium/low):')
 
 time_bound = input('Is it time-bound? (yes/no):')
 
-if time_bound == 'yes':
-    print(f'Reminder: {task} is a {priority} priority task that requires immediate attention today!')
-elif time_bound == 'no':
-    print(f'Note: {task} is a {priority} priority task. Consider completing it when you have free time.')
+
+match priority:
+    case 'high':
+        if time_bound == 'yes':
+            print(f'Reminder: {task} is a high priority task that requires immediate attention today!')
+        elif time_bound == 'no':
+            print(f'Note: {task} is a high priority task. Consider completing it when you have free time.')
+    case 'medium':
+        if time_bound == 'yes':
+            print(f'Reminder: {task} is a medium priority task that should be completed soon.')
+        elif time_bound == 'no':
+            print(f'Note: {task} is a medium priority task. You can schedule it for later.')
+    case 'low':
+        if time_bound == 'yes':
+            print(f'Reminder: {task} is a low priority task that can be done at your convenience.')
+        elif time_bound == 'no':
+            print(f'Note: {task} is a low priority task. It can be postponed without issues.')
+    case _:
+        if time_bound == 'yes':
+            print(f'Reminder: {task} is a low priority task that can be done at your convenience.')
+        elif time_bound == 'no':
+            print(f'Note: {task} is a low priority task. It can be postponed without issues.')
+
--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -3,14 +3,16 @@ num2 = int(input('Enter the second number:'))
 
 op = input('Choose the operation (+, -, *, /):')
 
-if op == '+':
-    print(f'The result is {num1 + num2}.')
-elif op == '-':
-    print(f'The result is {num1 - num2}.')
-elif op == '*':
-    print(f'The result is {num1 * num2}.')
-elif op == '/':
-    if num2 != 0:
-        print(f'The result is {num1 / num2}.')
-    else:
-        print("Cannot divide by zero.")
+
+match op:
+    case '+':
+        print(f'The result is {num1 + num2}.')
+    case '-':
+        print(f'The result is {num1 - num2}.')
+    case '*':
+        print(f'The result is {num1 * num2}.')
+    case '/':
+        if num2 != 0:
+            print(f'The result is {num1 / num2}.')
+        else:
+            print("Cannot divide by zero.")
