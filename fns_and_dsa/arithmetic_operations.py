7e900f0af77c0ea5cbc104a5cdb215714d79825d
ermias <ermiaskindalem97@gmail.com>
2025-07-24 17:43:39 +0300

    completed

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -8,7 +8,7 @@ def perform_operation(num1, num2, operation):
     elif operation == 'divide':
         if num2 != 0:
             return num1 / num2
-        else:
+        if num2 == 0:
             return "Error: Division by zero is not allowed."
     else:
         return "Error: Invalid operation"
